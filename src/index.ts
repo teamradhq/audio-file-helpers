037e39e670c5c5194b1c2c698a1b0962c98d8909
Paul Beynon <paul.beynon@gmail.com>
2021-03-04 15:14:29 +1100

    Add lib.matchMetaDataFromFileName

--- src/index.ts
+++ src/index.ts
@@ -35,86 +35,11 @@ const log = (item: unknown): void => console.log(inspect(item, {
 
 import { listAudioFiles } from '@lib/listAudioFiles';
 import { listSubDirectories } from '@lib/listSubDirectories';
-import { getMimeType } from '@lib/getMimeType';
-import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from 'constants';
-
-const patterns = {
-  trackNumber: '\\d{1,2}',
-  artist: '[A-Za-z0-9, ]*',
-  title: '[A-Za-z0-9,()"\' ]*',
-  key:'\\d{1,2}[AB]',
-  bpm:'\\d{2,3}',
-  separator: '\\s?-\\s?',
-};
-
-const mapped = Object.entries(patterns).reduce((parts: string[], [key, pattern]) => ([
-  ...parts,
-  key !== 'separator' ? `(?<${key}>${pattern})?` : `(${pattern})?`
-]), []);
-
-const separator = String(mapped.pop());
-
-const regExStr = mapped.join(separator).trim().slice(0, -1);
-
-const trackTagMatch = new RegExp(`^${regExStr}`);
-
-type AudioTagMeta = {
-  trackNumber?: string | number;
-  artist?: string;
-  title?: string;
-  key?: string;
-  bpm?: string;
-  separator?: string;
-}
-
-type MapperFunction = (key: string, value: string) => (string|number)[];
-
-type TagMapType = {
-  [key: string]: MapperFunction;
-}
-
-const tagMappers: TagMapType = {
-  DEFAULT: (key, value) => [key, (value || '').trim()],
-  artist: (key, value) => [
-    key,
-    value.split(',').map(artist => artist.trim())
-      .sort().join(', ')
-  ],
-  trackNumber: (key, value) => [key, Number.parseInt(value || '0', 10)],
-};
-
-/**
- * Checks a file name for any metadata matching this pattern:
- *
- *    "{trackNumber} - {artist} - {title} - {key} - {bpm}"
- *
- * Any matches are parsed to a new AudioTagMeta object.
- *
- * @param   {string}        filepath
- *
- * @return  {AudioTagMeta}
- */
-const matchMetaDataFromFileName = (filepath: string): AudioTagMeta => {
-  const match = filepath.match(trackTagMatch)?.groups || {}
-  const entries = Object.entries(match).map(([key, value]) => {
-      const map = tagMappers[key] || tagMappers.DEFAULT;
-      return map(key, value);
-    });
-
-  return Object.fromEntries(entries);
-}
+import { matchMetaDataFromFileName } from '@lib/matchMetaDataFromFileName';
 
 const directories = listSubDirectories(cwd);
 const audiofiles = listAudioFiles(directories);
-
-
-
-audiofiles.slice(0, 100).forEach((file) => {
+audiofiles.slice(-100).forEach((file) => {
   log(file);
   log(matchMetaDataFromFileName(file));
 });
-
-// const test = "Vanessa Daou - Once in a While (Charles Webster's Midnight Mix) - 2A - 125.aiff";
-// log(test);
-// log(matchMetaDataFromFileName(test));
-
---
+++ src/lib/matchMetaDataFromFileName.ts
@@ -0,0 +1,90 @@
+/**
+ * Audio file metadata, which may be present in its name.
+ */
+type AudioTagMeta = {
+  trackNumber?: string | number;
+  artist?: string;
+  title?: string;
+  key?: string;
+  bpm?: string;
+  separator?: string;
+}
+
+/**
+ * A function that maps an object entry's value.
+ */
+type EntryMapperFunction = (key: string, value: string) => (string|number)[];
+
+/**
+ * A collection of entry mappers, including
+ * a default mapper.
+ */
+type EntryMapperCollection = {
+  DEFAULT: EntryMapperFunction;
+  [key: string]: EntryMapperFunction;
+}
+
+/**
+ * RegExp patterns for identifying certain
+ * metadata from a string value.
+ *
+ * @var {AudioTagMeta}
+ */
+const patterns: Required<AudioTagMeta> = {
+  trackNumber: '\\d{1,2}',
+  artist: '[A-Za-z0-9, ]*',
+  title: '[A-Za-z0-9,()"\' ]*',
+  key:'\\d{1,2}[AB]',
+  bpm:'\\d{2,3}',
+  separator: '\\s?-\\s?',
+};
+
+/**
+ * Named, optional groups for each RegExp pattern.
+ */
+const patternRegExpGroups: string[] = Object.entries(patterns)
+  .reduce((parts: string[], [key, pattern]) => ([
+    ...parts,
+    key !== 'separator' ? `(?<${key}>${pattern})?` : `(${pattern})?`
+  ]), []);
+
+/**
+ * Pattern for matching string separators.
+ */
+const separator = String(patternRegExpGroups.pop());
+
+/**
+ * Regex should match types of metadata in order:
+ * {trackNumber} - {artist} - {title} - {key} - {bpm}
+ */
+const trackTagMatch = new RegExp(`^${patternRegExpGroups.join(separator).trim().slice(0, -1)}`);
+
+const tagMatchMappers: EntryMapperCollection = {
+  DEFAULT: (key, value) => [key, (value || '').trim()],
+  artist: (key, value) => [
+    key,
+    value.split(',').map(artist => artist.trim())
+      .sort().join(', ')
+  ],
+  trackNumber: (key, value) => [key, Number.parseInt(value || '0', 10)],
+};
+
+/**
+ * Checks a file name for any metadata matching this pattern:
+ *
+ *    "{trackNumber} - {artist} - {title} - {key} - {bpm}"
+ *
+ * Any matches are parsed to a new AudioTagMeta object.
+ *
+ * @param   {string}        filepath
+ *
+ * @return  {AudioTagMeta}
+ */
+export const matchMetaDataFromFileName = (filepath: string): AudioTagMeta => {
+  const match = filepath.match(trackTagMatch)?.groups || {}
+  const entries = Object.entries(match).map(
+    ([key, value]) => (tagMatchMappers[key] || tagMatchMappers.DEFAULT)(key, value)
+  );
+
+  return Object.fromEntries(entries);
+}
